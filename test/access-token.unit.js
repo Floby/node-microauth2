755d39bc483227ec675493d3dd6d117b15cae40f
Florent Jaby <florent.jaby@gmail.com>
2016-01-24 20:29:20 +0100

    create service to generate access token data

---
+++ test/access-token.unit.js
@@ -0,0 +1,65 @@
+var expect = require('chai').expect
+var AccessToken = require('../lib/access-token')
+var Clients = require('../lib/clients')
+var ClientsMock;
+var sinon = require('sinon');
+
+describe('AccessToken', function () {
+  beforeEach(() => ClientsMock = sinon.mock(Clients) )
+  afterEach(() => ClientsMock.restore())
+
+  describe('.generate(credentials)', function () {
+    describe('with no credentials', function () {
+      it('throws', function () {
+        expect(() => AccessToken.generate()).to.throw(Error)
+      })
+    })
+    describe('with client_credentials', function () {
+      describe('when the client_id and client_secret match', function () {
+        var actual
+        var credentials = {
+          grant_type: 'client_credentials',
+          client_id: 'my-id',
+          client_secret: 'my-secret',
+          scope: ['A']
+        }
+        var client = {id: 'my-id', secret: 'my-secret', scope: ['A', 'B']}
+        beforeEach(function () {
+          ClientsMock.expects('getById').withArgs('my-id').returns(client)
+          actual = AccessToken.generate(credentials)
+        })
+        afterEach(function () {
+          ClientsMock.verify()
+        })
+
+        it('returns an object', function () {
+          expect(actual).to.be.an('object')
+        })
+
+        it('has queried scope', function () {
+          expect(actual).to.have.property('scope')
+          expect(actual.scope).to.deep.equal(['A'])
+        })
+
+        it('has client id', function () {
+          expect(actual).to.have.property('cid')
+          expect(actual.cid).to.equal('my-id')
+        })
+      })
+
+      describe('when the client_id and client_secret do not match', function () {
+        var credentials = {
+          grant_type: 'client_credentials',
+          client_id: 'my-id',
+          client_secret: 'other'
+        }
+        it('throws', function () {
+          var client = {id: 'my-id', secret: 'my-secret'}
+          ClientsMock.expects('getById').withArgs('my-id').returns(client)
+          expect(() => AccessToken.generate(credentials)).to.throw(Error)
+          ClientsMock.verify()
+        })
+      })
+    })
+  })
+})
---
+++ test/clients.unit.js
@@ -0,0 +1,39 @@
+var sinon = require('sinon');
+var expect = require('chai').expect
+var Clients = require('../lib/clients');
+var config = require('../lib/config')
+
+describe('Clients', function () {
+  var clients = [
+    {
+      id: 'hello',
+      secret: 'secret',
+      scopes: ['one', 'or', 'two', 'scopes']
+    }
+  ]
+  beforeEach(function () {
+    sinon.stub(config, 'get').withArgs('clients').returns(clients)
+  })
+  afterEach(function () {
+    config.get.restore()
+  })
+  describe('getById(id)', function () {
+    describe('when the client does not exist', function () {
+      it('returns null', function () {
+        expect(Clients.getById('nothing')).to.be.null
+      })
+    })
+
+    describe('when the client exists', function () {
+      it('returns its record', function () {
+        var client = Clients.getById('hello')
+        expect(client).to.be.an('object')
+        expect(client).to.deep.equal({
+          id: 'hello',
+          secret: 'secret',
+          scopes: ['one', 'or', 'two', 'scopes']
+        })
+      })
+    })
+  })
+})
